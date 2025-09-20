03786e3bc7ba55f56168e5dca3ddd05ecd7cae85
Sanskar <gundesanskar71@gmail.com>
2025-09-20 22:18:51 +0530

    Add components to App.tsx

--- stake-gambling-website/frontend/src/App.tsx
+++ stake-gambling-website/frontend/src/App.tsx
@@ -1,17 +1,18 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {Simulation} from './components/Simulate';
+import {Simulation} from './components/Simulation';
 import {Game} from './components/Game';
-import {Navbar} from './components/Navbar';
+import {Footer,Navbar}  from "./components";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar/>
+    <Navbar/>
       <Routes>
         <Route path="/simulation" element={<Simulation/>}/>
         <Route path="/game" element={<Game/>}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
